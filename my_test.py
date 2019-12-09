ef9b04de8b2c1445a79ef07a7e3d3d59e8913ee3
白井優武 <masatakeshirai0814@gmail.com>
2019-12-10 03:05:11 +0900

    feat:スレッドの列挙とレジスタ状態の補足

--- my_test.py
+++ my_test.py
@@ -2,9 +2,27 @@ import my_debugger
 
 debugger = my_debugger.debugger()
 
-debugger.load("C:\\Windows\\System32\\notepad.exe")
+#debugger.load("C:\\Windows\\System32\\notepad.exe")
 
 pid = raw_input("Enter the PID of the process to attach to : ");
 debugger.attach(int(pid))
-debugger.run()
+#debugger.run()
+#debugger.detach()
+
+list = debugger.enumerate_threads()
+
+for thread in list:
+
+    thread_context = debugger.get_thread_context(thread)
+
+    print "[*] Dump registers for thread ID : 0x%08x" % thread
+    print "[**] EIP: 0x%08x" % thread_context.Eip
+    print "[**] ESP: 0x%08x" % thread_context.Esp
+    print "[**] EBP: 0x%08x" % thread_context.Ebp
+    print "[**] EAX: 0x%08x" % thread_context.Eax
+    print "[**] EBX: 0x%08x" % thread_context.Ebx
+    print "[**] ECX: 0x%08x" % thread_context.Ecx
+    print "[**] EDX: 0x%08x" % thread_context.Edx
+    print "END DUMP"
+
 debugger.detach()
